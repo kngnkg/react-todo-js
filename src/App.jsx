280570c9081610dd2c2b64b8adbcb968a9dcde9c
ryokawa <kawata.ryo.0518@gmail.com>
2022-12-23 08:43:00 +0900

    add delete/complete button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './styles.css'
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState([
     'ああああ',
     'いいいい'
@@ -10,22 +11,49 @@ export const App = () => {
     'うううう',
     'ええええ'
   ]);
-  
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    // 配列をコピー
+    const newTodos = [...incompleteTodos];
+    // 対象のインデックスの要素を削除
+    newTodos.splice(index, 1);
+    // 反映
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setcompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
     <div className="input-area">
-      <input placeholder="TODOを入力" />
-      <button>追加</button>
+      <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} />
+      <button onClick={onClickAdd}>追加</button>
     </div>
     <div className="incomplete-area">
       <p className="title">未完了のTODO</p>
       <ul>
-        {incompleteTodos.map((todo) => {
+        {incompleteTodos.map((todo, index) => {
           return (
             <div key={todo} className="list-row">
               <li>{todo}</li>
-              <button>完了</button>
-              <button>削除</button>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
             </div>
           );
         })}
